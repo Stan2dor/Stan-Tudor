78dbe35baae3bf81acaa8f9b85b49d4b360c7214
Stan T <stan@Stans-Mac-mini.local>
2021-07-04 14:41:47 -0700

    router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@
 
 import Navbar from "./navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className='App'>
       <Navbar />
       <div className='content'>
-        <Home />
+        <Switch>
+          <Route path="/"> 
+            <Home />
+          </Route>
+        </Switch>
       </div>
       <header className='App-header'>
         {/* <img src={logo} className='App-logo' alt='logo' /> */}
       </header>
     </div>
+    </Router>
   );
 }
 
