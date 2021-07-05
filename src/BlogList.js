5d802e018e43d90ac01c2d298529a9bbe513016e
Stan T <stan@Stans-Mac-mini.local>
2021-07-05 11:21:09 -0700

    cleanup

--- src/BlogList.js
+++ src/BlogList.js
@@ -5,16 +5,6 @@ const BlogList = ({ blogs, title, body }) => {
   //   const blogs = props.blogs; /* same thing as prop here
   //   const title = props.title; /* same thing as prop here
 
-  /* DELETE BLOG FUNCTION
-  -----------------------------------*/
-  const [props, setProps] = useState(blogs);
-  // const setData = new blogs();
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setProps(newBlogs);
-  };
-
   return (
     <div className='blog-list'>
       <h2>{title}</h2>
@@ -24,7 +14,6 @@ const BlogList = ({ blogs, title, body }) => {
           <Link to={`/blogs/${blog.id}`}>
             <h2> {blog.title} </h2>
             <p> {blog.body} </p>
-            {/* <button onClick={() => handleDelete(blog.id)}>delete blog</button> */}
           </Link>
         </div>
       ))}
