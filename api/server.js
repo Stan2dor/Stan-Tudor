411521d60db519833ed6fa8f0a29aac27381aba0
Stan T <stan@Stans-Mac-mini.local>
2021-07-16 09:31:04 -0700

    cleanup

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+// const morgan = require("morgan");
+const { logger } = require("./middleware/middleware");
+const usersRouter = require("./users/usersRoutes");
+const server = express();
+
+server.use(helmet());
+// server.use(morgan());
+server.use(express.json());
+server.use(logger);
+server.use("/api/users", usersRouter);
+
+// global middlewares and the user's router
+server.get("/", (req, res) => {
+  res.send("<h2> Surprise! Lets write some middleware here </h2>");
+});
+
+server.use(function (req, res) {
+  res.status(404).send("Aint nobody got time for that");
+});
+
+module.exports = server;
