39a0a88d56fe4ee3f6eeb0c87d5e703b522c5b21
Stan <stan2dor@gmail.com>
2021-07-29 11:39:21 -0700

    cleanup

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const db = require("../data/db-config");
 const App = require("./src/App");
 
 // const cors = require("cors");
 const helmet = require("helmet");
 // const morgan = require("morgan");
-// const { logger } = require("./middleware/middleware");
+// const postsRoutes = require("./posts/postsRoutes");
 // const usersRoutes = require("./users/usersRoutes");
 const server = express();
 
@@ -12,6 +13,7 @@ server.use(helmet());
 server.use(express.json());
 // server.use(morgan());
 // server.use(logger);
+// server.use("/api/posts", postsRoutes);
 // server.use("/api/users", usersRoutes);
 // server.use(cors);
 server.use(App);
@@ -25,7 +27,13 @@ server.get("/api", (req, res) => {
   res.json({ message: "api is working" });
 });
 server.get("/api/users", (req, res) => {
-  res.json({ message: "users is here" });
+  db("users")
+    .then((users) => {
+      res.json({ message: "users is here" });
+    })
+    .catch((err) => {
+      res.status(500).json({ err: "failed to get users" });
+    });
 });
 // server.use("*", (req, res) => {
 //   res.send("<h1>Hello there</h1>");
@@ -34,7 +42,7 @@ server.use((err, req, res, next) => {
   res.status(500).json({ message: err.message, stack: err.stack });
 });
 server.use(function (req, res) {
-  res.status(404).send("Aint nobody got time for that - page not found");
+  res.status(404).send(" 404 - page not found");
 });
 
 module.exports = server;
--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -1,7 +1,8 @@
 const knexCleaner = require("knex-cleaner");
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   return knexCleaner.clean(knex, {
-    ignoreTables: ["knex_migrations", "knex_migrations_lock"]
+    mode: "truncate", // resets id's
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"],
   });
 };
